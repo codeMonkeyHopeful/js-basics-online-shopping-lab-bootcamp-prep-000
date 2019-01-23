31f020c2f990070cdd5e6cd1913a271e06ff162a
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-23 12:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({'itemName': item, 'itemPrice': Math.floor(Math.random() * 100)})
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  
 }
 
 function total() {
-  // write your code here
+  var total = 0;
+  for(let i = 0; i < cart.length; i++) {
+    total += cart[i]['itemPrice'];
+  }
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var modCart = cart.splice();
+  for(let i = 0; i < cart.length; i++) {
+    if(cart[i]['itemName'] === item){
+      cart.splice(i,1);
+    }
+  }
+  if(modCart !== cart){
+  return cart;
+  } else { return 'That item isn not in your cart.'}
 }
 
 function placeOrder(cardNumber) {
